fb65b6e5c37364005edde855a27abf632bff49d5
Ha Jiwoon <juneha1120@gmail.com>
2025-01-19 16:06:03 +0800

    Add mark, unmark command to mark and unmark added tasks and add command to add task

--- src/main/java/Juno.java
+++ src/main/java/Juno.java
@@ -9,12 +9,16 @@ public class Juno {
         ui.showGreetingMessage();
         while (true) {
             // Read user input
-            input = scanner.nextLine();
+            input = scanner.nextLine().trim();
+            String[] command = input.split(" ", 2);
 
-            if (input.equalsIgnoreCase("bye")) {ui.showExitMessage(); break;}
-            else if (input.equalsIgnoreCase("help") || input.equalsIgnoreCase("juno")) ui.showHelp();
-            else if (input.equalsIgnoreCase("list") || input.equalsIgnoreCase("tasks")) ui.showTasks();
-            else ui.addTask(input);
+            if (command[0].equalsIgnoreCase("bye") || command[0].equalsIgnoreCase("exit")) {ui.showExitMessage(); break;}
+            else if (command[0].equalsIgnoreCase("juno") || command[0].equalsIgnoreCase("help")) ui.showHelp();
+            else if (command[0].equalsIgnoreCase("list") || command[0].equalsIgnoreCase("tasks")) ui.showTasks();
+            else if (command[0].equalsIgnoreCase("mark") && command.length > 1) ui.markTask(command[1]);
+            else if (command[0].equalsIgnoreCase("unmark") && command.length > 1) ui.unmarkTask(command[1]);
+            else if (command[0].equalsIgnoreCase("add") && command.length > 1) ui.addTask(command[1]);
+            else ui.echoInput(input);
         }
 
         scanner.close();
