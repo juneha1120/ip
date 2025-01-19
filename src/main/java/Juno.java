e2c66402ecc156fa10077eacbc6d12d81d8ac428
Ha Jiwoon <juneha1120@gmail.com>
2025-01-19 13:28:39 +0800

    Add greeting and exiting message

---
+++ src/main/java/Juno.java
@@ -0,0 +1,14 @@
+public class Juno {
+    public static void main(String[] args) {
+        // Print greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Juno.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Print exiting message
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
