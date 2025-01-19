35f7bbdda1a9f5b3a9a9e5db22217ffe73886e4b
Ha Jiwoon <juneha1120@gmail.com>
2025-01-19 14:07:01 +0800

    Separate Juno into two; Juno and JunoUI

--- src/main/java/Juno.java
+++ src/main/java/Juno.java
@@ -2,30 +2,23 @@ import java.util.*;
 
 public class Juno {
     public static void main(String[] args) {
-        // Print greeting message
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Juno.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-
+        JunoUI ui = new JunoUI();
         Scanner scanner = new Scanner(System.in);
-        String userInput;
+        String input;
+
+        ui.greet();
         while (true) {
             // Read user input
-            userInput = scanner.nextLine();
+            input = scanner.nextLine();
 
-            // Print exiting message when user input is exit command
-            if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+            // Exit if user input is "bye" (case-insensitive)
+            if (input.equalsIgnoreCase("bye")) {
+                ui.exit();
                 break;
             }
 
             // Echo user input
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + userInput);
-            System.out.println("____________________________________________________________");
+            ui.echoInput(input);
         }
 
         scanner.close();
---
+++ src/main/java/JunoUI.java
@@ -0,0 +1,23 @@
+public class JunoUI {
+    // Print greeting message
+    public void greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Juno.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    // Print exiting message
+    public void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    // Echo user input
+    public void echoInput(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" " + input);
+        System.out.println("____________________________________________________________");
+    }
+}
