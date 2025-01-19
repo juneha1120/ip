dd1b2b9c762c5747212cd7125b31f093a9993c16
Ha Jiwoon <juneha1120@gmail.com>
2025-01-19 13:56:51 +0800

    Add user input echoing feature

--- src/main/java/Juno.java
+++ src/main/java/Juno.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Juno {
     public static void main(String[] args) {
         // Print greeting message
@@ -6,9 +8,26 @@ public class Juno {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Print exiting message
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            // Read user input
+            userInput = scanner.nextLine();
+
+            // Print exiting message when user input is exit command
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            // Echo user input
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
