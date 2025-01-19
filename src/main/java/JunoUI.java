134b27fdb3e0dce5578d8fa9781f183bfd48db13
Ha Jiwoon <juneha1120@gmail.com>
2025-01-19 16:07:30 +0800

    Add markTask and unmarkTask to mark and unmark task and show appropriate message Edit showTasks to show status of task as well

--- src/main/java/JunoUI.java
+++ src/main/java/JunoUI.java
@@ -2,42 +2,90 @@ import java.util.*;
 
 public class JunoUI {
     private final Map<String, String> commands;
-    private Map<Integer, String> tasks;
-    private int taskCount = 0;
+    private Map<Integer, JunoTask> tasks;
+    private int taskNum = 0;
 
     public JunoUI() {
-        commands = new HashMap<>();
+        commands = new LinkedHashMap<>();
         initCommands();
-        tasks = new HashMap<>();
-        initTasks();
+        tasks = new LinkedHashMap<>();
+        taskNum++;
     }
 
     public void initCommands() {
         // Add commands and their descriptions
-        commands.put("bye", "exits the chatbot.");
-        commands.put("juno OR help", "shows available commands.");
-        commands.put("list OR tasks", "shows list of added tasks.");
-    }
-
-    public void initTasks() {
-        tasks = new HashMap<>();
-        taskCount++;
+        commands.put("bye (or exit)", "exits the chatbot.");
+        commands.put("juno (or help)", "shows available commands.");
+        commands.put("list (or tasks)", "shows list of added tasks.");
     }
 
     // Adds task to tasks
     public void addTask(String input) {
-        tasks.put(taskCount++, input);
+        tasks.put(taskNum, new JunoTask(input));
         System.out.println("____________________________________________________________");
-        System.out.println(" Added : " + input);
+        System.out.println(" Added : " + taskNum + ". " + input);
         System.out.println("____________________________________________________________");
+        taskNum++;
+    }
+
+    // Mark task as done
+    public void markTask(String command) {
+        try {
+            int taskNum = Integer.parseInt(command);
+            if (tasks.containsKey(taskNum)) {
+                JunoTask curr = tasks.get(taskNum);
+                curr.mark();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done :");
+                System.out.println(" " + curr.getStatusIcon() + curr.getDescription());
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" I don't seem to find that task.");
+                System.out.println("____________________________________________________________");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" I don't seem to find that task.");
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    // Unmark task
+    public void unmarkTask(String command) {
+        try {
+            int taskNum = Integer.parseInt(command);
+            if (tasks.containsKey(taskNum)) {
+                JunoTask curr = tasks.get(taskNum);
+                curr.unmark();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Ok, I've marked this task as not done yet :");
+                System.out.println(" " + curr.getStatusIcon() + curr.getDescription());
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" I don't seem to find that task.");
+                System.out.println("____________________________________________________________");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" I don't seem to find that task.");
+            System.out.println("____________________________________________________________");
+        }
     }
 
     // Print tasks
     public void showTasks() {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Here's what you have :");
-        tasks.forEach((num, task) -> System.out.println(num + ". " + task));
-        System.out.println("____________________________________________________________");
+        if (taskNum > 1) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Here's what you have :");
+            tasks.forEach((taskNum, task) -> System.out.println(taskNum + ". " + task.getStatusIcon() + task.getDescription()));
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Looks like you have nothing! ");
+            System.out.println("____________________________________________________________");
+        }
     }
 
     // Print greeting message
@@ -65,12 +113,10 @@ public class JunoUI {
         System.out.println("____________________________________________________________");
     }
 
-//    // Echo user input
-//    public void echoInput(String input) {
-//        System.out.println("____________________________________________________________");
-//        System.out.println(" " + input);
-//        System.out.println("____________________________________________________________");
-//    }
-
-
+    // Echo user input
+    public void echoInput(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" " + input);
+        System.out.println("____________________________________________________________");
+    }
 }
