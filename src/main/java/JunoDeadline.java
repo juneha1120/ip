ac500f266555c2db341cd71234e185e389a8f465
Ha Jiwoon <juneha1120@gmail.com>
2025-01-26 15:46:46 +0800

    Add toFileFormat() method for JunoTodo, JunoDeadline, JunoEvent
    They do not have method to yield appropriate format for saving
    
    To save each task in text file, requires toFileFormat() method
    
    Add toFileFormat() method to get task type, status and its description
    as string supported for saving

--- src/main/java/JunoDeadline.java
+++ src/main/java/JunoDeadline.java
@@ -6,6 +6,16 @@ public class JunoDeadline extends JunoTask{
         this.by = by;
     }
 
+    public JunoDeadline(String description, boolean isDone, String by) {
+        super(description, isDone);
+        this.by = by;
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "D | " + (isDone() ? "1" : "0") + " | " + this.getDescription() + " | " + this.by;
+    }
+
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by : " + by + ")";
--- src/main/java/JunoTodo.java
+++ src/main/java/JunoTodo.java
@@ -3,6 +3,15 @@ public class JunoTodo extends JunoTask {
         super(description);
     }
 
+    public JunoTodo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "T | " + (isDone() ? "1" : "0") + " | " + this.getDescription();
+    }
+
     @Override
     public String toString() {
         return "[T]" + super.toString();
