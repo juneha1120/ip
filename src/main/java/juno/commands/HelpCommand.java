455d35607cbaea79203037c3a0ea71098d7d97fb
Ha Jiwoon <juneha1120@gmail.com>
2025-01-28 13:48:38 +0800

    HelpCommand.java : Revert COMMAND_WORD string that was accidentally changed while refactoring

--- src/main/java/juno/commands/HelpCommand.java
+++ src/main/java/juno/commands/HelpCommand.java
@@ -7,7 +7,7 @@ import juno.storage.Storage;
 import juno.ui.Ui;
 
 public class HelpCommand extends Command {
-    public static final String COMMAND_WORD = "main";
+    public static final String COMMAND_WORD = "juno";
     public static final String COMMAND_DESCRIPTION = COMMAND_WORD + " : shows list of available commands.\n"
             + " ".repeat(COMMAND_WORD.length()) + EXAMPLE_PREFIX + COMMAND_WORD;
 
