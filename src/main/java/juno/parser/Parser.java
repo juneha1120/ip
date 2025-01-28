db7bb0395a9ba133201a113ae9ac9476b3a1000c
Ha Jiwoon <juneha1120@gmail.com>
2025-01-28 18:59:16 +0800

    Tweak Parser.java to comply with coding standard

--- src/main/java/juno/parser/Parser.java
+++ src/main/java/juno/parser/Parser.java
@@ -1,29 +1,45 @@
 package juno.parser;
 
-import juno.commands.*;
-
-import juno.exceptions.JunoException;
-
-import juno.exceptions.JunoTestException;
-import juno.task.Todo;
-import juno.task.Deadline;
-import juno.task.Event;
+import static juno.enums.ErrorType.DATE_ERROR;
+import static juno.enums.ErrorType.DEADLINE_ERROR;
+import static juno.enums.ErrorType.DELETE_ERROR;
+import static juno.enums.ErrorType.EVENT_ERROR;
+import static juno.enums.ErrorType.MARK_ERROR;
+import static juno.enums.ErrorType.TASK_ERROR;
+import static juno.enums.ErrorType.TODO_ERROR;
+import static juno.enums.ErrorType.UNMARK_ERROR;
+import static juno.enums.TaskType.DEADLINE;
+import static juno.enums.TaskType.EVENT;
+import static juno.enums.TaskType.TODO;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+import juno.commands.AddCommand;
+import juno.commands.Command;
+import juno.commands.DeleteCommand;
+import juno.commands.ExitCommand;
+import juno.commands.HelpCommand;
+import juno.commands.MarkCommand;
+import juno.commands.ShowTasksCommand;
+import juno.commands.ShowTasksWithDateCommand;
+import juno.commands.TestCommand;
+import juno.commands.TryAgainCommand;
+import juno.commands.UnmarkCommand;
 import juno.enums.TaskType;
-
-import static juno.enums.ErrorType.*;
-import static juno.enums.TaskType.*;
+import juno.exceptions.JunoException;
+import juno.exceptions.JunoTestException;
+import juno.task.Deadline;
+import juno.task.Event;
+import juno.task.Todo;
 
 public class Parser {
     public Command parse(String fullCommand) throws JunoException {
         String[] command = fullCommand.split(" ", 2);
         if (command.length < 2) {
-            command = new String[] {command[0], ""};
+            command = new String[]{command[0], ""};
         }
 
         String commandWord = command[0];
@@ -37,32 +53,20 @@ public class Parser {
             }
         }
 
-        switch (commandWord) {
-            case HelpCommand.COMMAND_WORD :
-                return new HelpCommand();
-            case Todo.COMMAND_WORD :
-                return prepareAddCommand(TODO, arguments);
-            case Deadline.COMMAND_WORD :
-                return prepareAddCommand(DEADLINE, arguments);
-            case Event.COMMAND_WORD :
-                return prepareAddCommand(EVENT, arguments);
-            case ShowTasksCommand.COMMAND_WORD :
-                return new ShowTasksCommand();
-            case ShowTasksWithDateCommand.COMMAND_WORD :
-                return prepareShowWithDateCommand(arguments);
-            case MarkCommand.COMMAND_WORD :
-                return prepareMarkCommand(arguments);
-            case UnmarkCommand.COMMAND_WORD :
-                return prepareUnmarkCommand(arguments);
-            case DeleteCommand.COMMAND_WORD :
-                return prepareDeleteCommand(arguments);
-            case ExitCommand.COMMAND_WORD :
-                return new ExitCommand();
-            case TestCommand.COMMAND_WORD, TestCommand.COMMAND_LINE, TestCommand.EMPTY_LINE:
+        return switch (commandWord) {
+        case HelpCommand.COMMAND_WORD -> new HelpCommand();
+        case Todo.COMMAND_WORD -> prepareAddCommand(TODO, arguments);
+        case Deadline.COMMAND_WORD -> prepareAddCommand(DEADLINE, arguments);
+        case Event.COMMAND_WORD -> prepareAddCommand(EVENT, arguments);
+        case ShowTasksCommand.COMMAND_WORD -> new ShowTasksCommand();
+        case ShowTasksWithDateCommand.COMMAND_WORD -> prepareShowWithDateCommand(arguments);
+        case MarkCommand.COMMAND_WORD -> prepareMarkCommand(arguments);
+        case UnmarkCommand.COMMAND_WORD -> prepareUnmarkCommand(arguments);
+        case DeleteCommand.COMMAND_WORD -> prepareDeleteCommand(arguments);
+        case ExitCommand.COMMAND_WORD -> new ExitCommand();
+        case TestCommand.COMMAND_WORD, TestCommand.COMMAND_LINE, TestCommand.EMPTY_LINE ->
                 throw new JunoTestException();
-            default:
-                return new TryAgainCommand();
-        }
+        default -> new TryAgainCommand(); };
     }
 
     protected boolean isTimeArgument(String argument) {
@@ -71,98 +75,99 @@ public class Parser {
 
     protected Command prepareAddCommand(TaskType taskType, String arguments) throws JunoException {
         switch (taskType) {
-            case TODO :
-                if (arguments.isEmpty()) {
-                    throw new JunoException(TODO_ERROR);
+        case TODO:
+            if (arguments.isEmpty()) {
+                throw new JunoException(TODO_ERROR);
+            } else {
+                return new AddCommand(new Todo(arguments));
+            }
+        case DEADLINE:
+            if (arguments.isEmpty()) {
+                throw new JunoException(DEADLINE_ERROR, false);
+            } else if (!arguments.contains("/by")) {
+                throw new JunoException(DEADLINE_ERROR, true);
+            } else {
+                String[] argumentSplit = arguments.split("/by", 2);
+                String taskName = argumentSplit[0].trim();
+                String taskBy = argumentSplit[1].trim();
+                DateTimeFormatter byFormatter;
+                if (this.isTimeArgument(taskBy)) {
+                    byFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
+                    try {
+                        LocalDateTime by = LocalDateTime.parse(taskBy, byFormatter);
+                        return new AddCommand(new Deadline(taskName, by));
+                    } catch (DateTimeParseException e) {
+                        throw new JunoException(DEADLINE_ERROR, true);
+                    }
                 } else {
-                    return new AddCommand(new Todo(arguments));
+                    byFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+                    try {
+                        LocalDate by = LocalDate.parse(taskBy, byFormatter);
+                        return new AddCommand(new Deadline(taskName, by));
+                    } catch (DateTimeParseException e) {
+                        throw new JunoException(DEADLINE_ERROR, true);
+                    }
                 }
-            case DEADLINE :
-                if (arguments.isEmpty()) {
-                    throw new JunoException(DEADLINE_ERROR, false);
-                } else if (!arguments.contains("/by")) {
-                    throw new JunoException(DEADLINE_ERROR, true);
-                } else {
-                    String[] argumentSplitted = arguments.split("/by", 2);
-                    String taskName = argumentSplitted[0].trim();
-                    String taskBy = argumentSplitted[1].trim();
-                    DateTimeFormatter byFormatter;
-                    if (this.isTimeArgument(taskBy)) {
-                        byFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
+            }
+        case EVENT:
+            if (arguments.isEmpty()) {
+                throw new JunoException(EVENT_ERROR, false);
+            } else if (!arguments.contains("/from") || !arguments.contains("/to")) {
+                throw new JunoException(EVENT_ERROR, true);
+            } else {
+                String[] argumentSplit = arguments.split("/from", 2);
+                String[] fromTo = argumentSplit[1].split("/to", 2);
+                String taskName = argumentSplit[0].trim();
+                String taskFrom = fromTo[0].trim();
+                String taskTo = fromTo[1].trim();
+                DateTimeFormatter fromFormatter;
+                DateTimeFormatter toFormatter;
+                if (this.isTimeArgument(taskFrom)) {
+                    fromFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
+                    if (this.isTimeArgument(taskTo)) {
+                        toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
                         try {
-                            LocalDateTime by = LocalDateTime.parse(taskBy, byFormatter);
-                            return new AddCommand(new Deadline(taskName, by));
+                            LocalDateTime from = LocalDateTime.parse(taskFrom, fromFormatter);
+                            LocalDateTime to = LocalDateTime.parse(taskTo, toFormatter);
+                            return new AddCommand(new Event(taskName, from, to));
                         } catch (DateTimeParseException e) {
-                            throw new JunoException(DEADLINE_ERROR, true);
+                            throw new JunoException(EVENT_ERROR, true);
                         }
                     } else {
-                        byFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+                        toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
                         try {
-                            LocalDate by = LocalDate.parse(taskBy, byFormatter);
-                            return new AddCommand(new Deadline(taskName, by));
+                            LocalDateTime from = LocalDateTime.parse(taskFrom, fromFormatter);
+                            LocalDate to = LocalDate.parse(taskTo, toFormatter);
+                            return new AddCommand(new Event(taskName, from, to));
                         } catch (DateTimeParseException e) {
-                            throw new JunoException(DEADLINE_ERROR, true);
+                            throw new JunoException(EVENT_ERROR, true);
+                        }
+                    }
+                } else {
+                    fromFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+                    if (this.isTimeArgument(taskTo)) {
+                        toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
+                        try {
+                            LocalDate from = LocalDate.parse(taskFrom, fromFormatter);
+                            LocalDateTime to = LocalDateTime.parse(taskTo, toFormatter);
+                            return new AddCommand(new Event(taskName, from, to));
+                        } catch (DateTimeParseException e) {
+                            throw new JunoException(EVENT_ERROR, true);
+                        }
+                    } else {
+                        toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+                        try {
+                            LocalDate from = LocalDate.parse(taskFrom, fromFormatter);
+                            LocalDate to = LocalDate.parse(taskTo, toFormatter);
+                            return new AddCommand(new Event(taskName, from, to));
+                        } catch (DateTimeParseException e) {
+                            throw new JunoException(EVENT_ERROR, true);
                         }
                     }
                 }
-            case EVENT :
-                if (arguments.isEmpty()) {
-                    throw new JunoException(EVENT_ERROR, false);
-                } else if (!arguments.contains("/from") || !arguments.contains("/to") ) {
-                    throw new JunoException(EVENT_ERROR, true);
-                } else {
-                    String[] argumentSplitted = arguments.split("/from", 2);
-                    String[] fromTo = argumentSplitted[1].split("/to", 2);
-                    String taskName = argumentSplitted[0].trim();
-                    String taskFrom = fromTo[0].trim();
-                    String taskTo = fromTo[1].trim();
-                    DateTimeFormatter fromFormatter, toFormatter;
-                    if (this.isTimeArgument(taskFrom)) {
-                        fromFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
-                        if (this.isTimeArgument(taskTo)) {
-                            toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
-                            try {
-                                LocalDateTime from = LocalDateTime.parse(taskFrom, fromFormatter);
-                                LocalDateTime to = LocalDateTime.parse(taskTo, toFormatter);
-                                return new AddCommand(new Event(taskName, from, to));
-                            } catch (DateTimeParseException e) {
-                                throw new JunoException(EVENT_ERROR, true);
-                            }
-                        } else {
-                            toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-                            try {
-                                LocalDateTime from = LocalDateTime.parse(taskFrom, fromFormatter);
-                                LocalDate to = LocalDate.parse(taskTo, toFormatter);
-                                return new AddCommand(new Event(taskName, from, to));
-                            } catch (DateTimeParseException e) {
-                                throw new JunoException(EVENT_ERROR, true);
-                            }
-                        }
-                    } else {
-                        fromFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-                        if (this.isTimeArgument(taskTo)) {
-                            toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy H:mm");
-                            try {
-                                LocalDate from = LocalDate.parse(taskFrom, fromFormatter);
-                                LocalDateTime to = LocalDateTime.parse(taskTo, toFormatter);
-                                return new AddCommand(new Event(taskName, from, to));
-                            } catch (DateTimeParseException e) {
-                                throw new JunoException(EVENT_ERROR, true);
-                            }
-                        } else {
-                            toFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-                            try {
-                                LocalDate from = LocalDate.parse(taskFrom, fromFormatter);
-                                LocalDate to = LocalDate.parse(taskTo, toFormatter);
-                                return new AddCommand(new Event(taskName, from, to));
-                            } catch (DateTimeParseException e) {
-                                throw new JunoException(EVENT_ERROR, true);
-                            }
-                        }
-                    }
-                }
-            default :
-                throw new JunoException(TASK_ERROR);
+            }
+        default:
+            throw new JunoException(TASK_ERROR);
         }
     }
 
