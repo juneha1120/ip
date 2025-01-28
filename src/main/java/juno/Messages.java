a7efeb2010dc433a60d08625da9427622fcc753a
Ha Jiwoon <juneha1120@gmail.com>
2025-01-28 13:20:16 +0800

    Messages.java : Add string messages needed for Ui.java

---
+++ src/main/java/juno/Messages.java
@@ -0,0 +1,16 @@
+package juno;
+
+public class Messages {
+    public static final String MESSAGE_HELLO= "Hello! I'm Juno. What can I do for you?";
+    public static final String MESSAGE_LOAD = "I'm retrieving your saved task list from last time...";
+    public static final String MESSAGE_BYE = "Bye.. Hope to see you again soon!";
+    public static final String MESSAGE_CALL = "Call my name if you need help.";
+    public static final String MESSAGE_HELP = "You called? Here's what I can do.";
+    public static final String MESSAGE_TRY_AGAIN = "I'm sorry. Could you try again?";
+    public static final String MESSAGE_ADD_START = "Got it. I've added this task :";
+    public static final String MESSAGE_SHOW_TASKS_START = "Here's what you have :";
+    public static final String MESSAGE_NO_TASKS = "Looks like you have nothing in the list!";
+    public static final String MESSAGE_MARK_START = "Nice! I've marked this task as done :";
+    public static final String MESSAGE_UNMARK_START = "Ok, I've marked this task as not done yet :";
+    public static final String MESSAGE_DELETE_START = "Noted. I've removed this task :";
+}
