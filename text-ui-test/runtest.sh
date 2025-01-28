c5576c6b7591561d69bd811064c34d7167c83e70
Ha Jiwoon <juneha1120@gmail.com>
2025-01-28 13:28:50 +0800

    input.txt : Add commenting lines to better understand inputs EXPECTED.TXT : Add corresponding expected outputs

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,21 @@ then
     rm ACTUAL.TXT
 fi
 
+# delete saved files from previous run
+if [ -e "./data/juno.txt" ]
+then
+    rm data/juno.txt
+fi
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/juno/Juno.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Juno < input.txt > ACTUAL.TXT
+java -classpath ../bin juno.Juno < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
